f14d591719cb5c24f75125967f64c57c235e66cb
muratsat <muratsat@users.noreply.github.com>
2021-11-08 19:33:30 +0300

    fixed memory errors in initialization

--- homeTask1/bn.c
+++ homeTask1/bn.c
@@ -3,11 +3,10 @@
 #include <string.h>
 #include "bn.h"
 
-#define MAXSIZE 100000
-#define MOD 4294967296
+#define MOD 4294967296 // 2^32
 
 struct bn_s{
-    unsigned int digit[MAXSIZE];
+    unsigned int *digit;
     int size;
     int sign;
 };
@@ -15,19 +14,30 @@ typedef struct bn_s bn;
 
 bn *bn_new(){
     bn* bignum = (bn*)malloc(sizeof(bn));
-    bignum->size = 0;
+    bignum->size = 1;
     bignum->sign = 0;
+    bignum->digit = NULL;
+    bignum->digit = (unsigned int*)malloc(sizeof(unsigned int));
     bignum->digit[0] = 0;
     return bignum;
 }
 
 bn *bn_init(bn const *orig){
-    bn* copy = (bn*)malloc(sizeof(bn));
-    *copy = *orig;
+    bn* copy = bn_new();
+    copy->sign = orig->sign;
+    copy->size = orig->size;
+
+    for(int i = 0; i < orig->size; i++)
+        copy->digit[i] = orig->digit[i];
+
     return copy;
 }
 
 int bn_delete(bn *t){
+    if(t == NULL)
+        return 0;
+    if(t->digit != NULL) 
+        free(t->digit);
     free(t);
     return 0;
 }
