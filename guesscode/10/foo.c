0aaf71b60d03bdfb3857c21c0e8a9faea90d4bd0
muratsat <muratsat@users.noreply.github.com>
2022-05-07 10:59:35 +0300

    guessCode 10 solution

--- guesscode/10/foo.c
+++ guesscode/10/foo.c
@@ -1,17 +1,9 @@
-void foo(double* rdi, double* rsi){
-    double x1 = *rsi;
-    double x0 = 0;
-    if(x1 <= x0)
-        goto L3;
-    rsi++;
-L2:
-    x1 += *rdi;
-    *rdi = x1;
-    x1 = *rsi;
-    rsi++;
-    if(x1 > x0)
-        goto L2;
-L3:
-    return;
-
+void foo(double *x, double a[]){
+    double prev = a[0];
+    while(prev > 0){
+        a++;
+        prev += *x;
+        *x = prev;
+        prev = *a;
+    }
 }
